45c1bf9301289b26d3e7438521c6c556c95848d4
Wolfman™ <vn.sieu@gmail.com>
2017-11-29 00:26:30 +0700

    .

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,39 @@
 <?php
+define( 'WP_HOME', 'http://localhost/riverside' );
+define( 'WP_SITEURL', 'http://localhost/riverside' );
+define( 'UPLOADS', 'uploads' );
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/storage' );
+define( 'WP_CONTENT_URL', WP_SITEURL . '/storage' );
+
+/* WordPress debug mode for developers. */
+define( 'WP_DEBUG', false );
+define( 'WP_DEBUG_LOG', false );
+define( 'WP_DEBUG_DISPLAY', false );
+define( 'SCRIPT_DEBUG', false );
+define( 'SAVEQUERIES', false );
+
+/* WordPress Cache */
+define( 'WP_CACHE', true );
+
+
+/* Compression */
+define( 'COMPRESS_CSS', true );
+define( 'COMPRESS_SCRIPTS', true );
+define( 'CONCATENATE_SCRIPTS', true );
+define( 'ENFORCE_GZIP', true );
+
+/* Updates */
+define( 'WP_AUTO_UPDATE_CORE', false );
+define( 'DISALLOW_FILE_MODS', true );
+define( 'DISALLOW_FILE_EDIT', true );
+define( 'FS_CHMOD_DIR', ( 0755 & ~ umask() ) );
+define( 'FS_CHMOD_FILE', ( 0644 & ~ umask() ) );
+define( 'DISABLE_WP_CRON', true );
+define( 'EMPTY_TRASH_DAYS', 30 ); // 30 days
+
+/* Performance */
+define( 'WP_POST_REVISIONS', false );
+
 /**
  * The base configuration for WordPress
  *
@@ -20,22 +55,22 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'riverside');
+define( 'DB_NAME', 'riverside' );
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define( 'DB_PASSWORD', 'root' );
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+define( 'DB_COLLATE', 'utf8mb4_unicode_ci' );
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -46,14 +81,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'tqW{~/.9,6D6$~A+C I@n,gxE;$lseXxLj ,J{raQ/}c;$k.maxpo!MaeFL1k8q5');
-define('SECURE_AUTH_KEY',  'e)N]]7}VZiRAdsoK/`<NnYH?S?>[,h.3e+3T`nm8Pu=fZ$3`fa+K}m#xWCmz<kfN');
-define('LOGGED_IN_KEY',    'V0Ry/XsZhlIY!3y*xDgSlT4x;$1q4<xP9vu1Wd[K <~{j0 %+xV3I_@8QJ$t:Zf%');
-define('NONCE_KEY',        'nu:wxd;7N&C<mJ`gAQxaRiKv|CVGX.D/Z&rFQI!xFZm#*:SR5fY^LIY^1,|+#V+t');
-define('AUTH_SALT',        '2SP}{GJ~sWyo*fL9i6tG8m-?`|Q3Z9LmVU4Ge+kdW(`)3> :Zg~oC&o?,j(wQMvE');
-define('SECURE_AUTH_SALT', 'C`z+SBX6{:sCptmr8T8PE=:V>N7`cokNQf,r}?ny i!%<@]#~%xTL)bO5SVz.$:#');
-define('LOGGED_IN_SALT',   '3M_g]nQr4s+e2E/,`sQ3p1oqbUK|OeT:t,]Z}g+[1Ef!Ly(M5P$s&jjnl#mpBzz ');
-define('NONCE_SALT',       'H|$1*BnSONIi0=Gdhbgq(2h(rvP?W9BN&_0E.*!<dm`@cUg,)u:NAyQg5[$t@IFB');
+define( 'AUTH_KEY', 'tqW{~/.9,6D6$~A+C I@n,gxE;$lseXxLj ,J{raQ/}c;$k.maxpo!MaeFL1k8q5' );
+define( 'SECURE_AUTH_KEY', 'e)N]]7}VZiRAdsoK/`<NnYH?S?>[,h.3e+3T`nm8Pu=fZ$3`fa+K}m#xWCmz<kfN' );
+define( 'LOGGED_IN_KEY', 'V0Ry/XsZhlIY!3y*xDgSlT4x;$1q4<xP9vu1Wd[K <~{j0 %+xV3I_@8QJ$t:Zf%' );
+define( 'NONCE_KEY', 'nu:wxd;7N&C<mJ`gAQxaRiKv|CVGX.D/Z&rFQI!xFZm#*:SR5fY^LIY^1,|+#V+t' );
+define( 'AUTH_SALT', '2SP}{GJ~sWyo*fL9i6tG8m-?`|Q3Z9LmVU4Ge+kdW(`)3> :Zg~oC&o?,j(wQMvE' );
+define( 'SECURE_AUTH_SALT', 'C`z+SBX6{:sCptmr8T8PE=:V>N7`cokNQf,r}?ny i!%<@]#~%xTL)bO5SVz.$:#' );
+define( 'LOGGED_IN_SALT', '3M_g]nQr4s+e2E/,`sQ3p1oqbUK|OeT:t,]Z}g+[1Ef!Ly(M5P$s&jjnl#mpBzz ' );
+define( 'NONCE_SALT', 'H|$1*BnSONIi0=Gdhbgq(2h(rvP?W9BN&_0E.*!<dm`@cUg,)u:NAyQg5[$t@IFB' );
 
 /**#@-*/
 
@@ -63,7 +98,7 @@ define('NONCE_SALT',       'H|$1*BnSONIi0=Gdhbgq(2h(rvP?W9BN&_0E.*!<dm`@cUg,)u:N
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wf_';
+$table_prefix = 'wf_';
 
 /**
  * For developers: WordPress debugging mode.
@@ -77,13 +112,14 @@ $table_prefix  = 'wf_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+define( 'WP_DEBUG', false );
 
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
 
 /** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+require_once( ABSPATH . 'wp-settings.php' );
